7e31b1a384e2fb1e0f6c0ddada2a699ab4a60462
NiradaSukchit <59160104@go.buu.ac.th>
2019-10-13 17:28:20 +0700

    HomeFragment and AboutFragment

---
+++ app/src/main/java/buu/informatics/s59160104/areacalculator/MainActivity.kt
@@ -0,0 +1,14 @@
+package buu.informatics.s59160104.areacalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageButton
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_about)
+
+    }
+}
